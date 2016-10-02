57584c98fcd4fbe4ae94585581c3a48aa9ff0af2
Aurélien Manteaux <amanteaux@gmail.com>
2016-10-02 16:41:36 +0200

    Installation of Wisp module

--- src/main/java/com/coreoz/plume/file/guice/GuiceFileModule.java
+++ src/main/java/com/coreoz/plume/file/guice/GuiceFileModule.java
@@ -7,6 +7,7 @@ import com.coreoz.plume.file.services.file.FileServiceDb;
 import com.coreoz.plume.file.services.hash.ChecksumService;
 import com.coreoz.plume.file.services.hash.ChecksumServiceSha1;
 import com.coreoz.plume.file.services.scheduled.ScheduledTasks;
+import com.coreoz.wisp.guice.GuiceWispSchedulerModule;
 import com.google.inject.AbstractModule;
 
 public class GuiceFileModule extends AbstractModule {
@@ -17,6 +18,7 @@ public class GuiceFileModule extends AbstractModule {
 		bind(FileService.class).to(FileServiceDb.class);
 		bind(FileCacheService.class).to(FileCacheServiceGuava.class);
 
+		install(new GuiceWispSchedulerModule());
 		bind(ScheduledTasks.class).asEagerSingleton();
 	}
 
