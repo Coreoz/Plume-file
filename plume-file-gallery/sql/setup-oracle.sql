a67768ca5e92674b1fc9aeae295b70e6972c8415
Aurélien Manteaux <amanteaux@coreoz.com>
2016-11-10 18:04:38 +0100

    Oracle primary keys should be named

--- plume-file-gallery/sql/setup-oracle.sql
+++ plume-file-gallery/sql/setup-oracle.sql
@@ -1,9 +1,8 @@
-
 CREATE TABLE  plm_file_gallery (
   id_file NUMBER(19,0) NOT NULL,
   id_data NUMBER(19,0) NOT NULL,
   gallery_type varchar(255) NOT NULL,
   position NUMBER(8) NOT NULL,
-  PRIMARY KEY  (id),
-  CONSTRAINT plm_file_gallery FOREIGN KEY (id_file) REFERENCES plm_file (id)
+  CONSTRAINT plm_file_gallery_pk PRIMARY KEY  (id_file),
+  CONSTRAINT plm_file_gallery_fk FOREIGN KEY (id_file) REFERENCES plm_file (id)
 );
