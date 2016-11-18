d29bc77b5d40a3777cc11c166b859e27f94515a5
Aurélien Manteaux <amanteaux@coreoz.com>
2016-11-18 20:16:28 +0100

    Refactoring to be used with an enum and lombok

--- plume-file-gallery/src/main/java/com/coreoz/plume/file/gallery/webservices/permissions/FileGalleryTypeAdmin.java
+++ plume-file-gallery/src/main/java/com/coreoz/plume/file/gallery/webservices/permissions/FileGalleryTypeAdmin.java
@@ -3,15 +3,15 @@ package com.coreoz.plume.file.gallery.webservices.permissions;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
-import com.coreoz.plume.admin.security.permission.WebSessionPermission;
+import com.coreoz.plume.admin.jersey.WebSessionPermission;
 import com.coreoz.plume.file.gallery.services.gallery.FileGalleryType;
 
 public interface FileGalleryTypeAdmin extends FileGalleryType {
 
-	String galleryPermission();
+	String getGalleryPermission();
 
-	BiPredicate<WebSessionPermission, Long> isAllowedToChangeGallery();
+	BiPredicate<WebSessionPermission, Long> getAllowedToChangeGallery();
 
-	Predicate<String> isFilenameAllowed();
+	Predicate<String> getFilenameAllowed();
 
 }
