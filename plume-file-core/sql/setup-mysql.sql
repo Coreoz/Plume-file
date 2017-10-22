df1a006a69888df28c5c411305249923ff5cd9ed
Aurélien Manteaux <amanteaux@gmail.com>
2017-10-22 18:14:05 +0200

    Make sure the table name is recognized on linux

--- plume-file-core/sql/setup-mysql.sql
+++ plume-file-core/sql/setup-mysql.sql
@@ -1,5 +1,5 @@
-DROP TABLE IF EXISTS `plm_file`;
-CREATE TABLE  `plm_file` (
+DROP TABLE IF EXISTS `PLM_FILE`;
+CREATE TABLE  `PLM_FILE` (
   `id` bigint(20) NOT NULL,
   `filename` varchar(255) NULL,
   `file_type` varchar(255) NOT NULL,
