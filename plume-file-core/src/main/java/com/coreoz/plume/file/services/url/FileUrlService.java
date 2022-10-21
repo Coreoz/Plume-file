3bddc67e7144ae4ea1bb05191f86fd06b4fd37f4
Aurélien Manteaux <amanteaux@coreoz.com>
2022-10-21 19:19:36 +0200

    Add comment

--- plume-file-core/src/main/java/com/coreoz/plume/file/services/url/FileUrlService.java
+++ plume-file-core/src/main/java/com/coreoz/plume/file/services/url/FileUrlService.java
@@ -1,5 +1,10 @@
 package com.coreoz.plume.file.services.url;
 
+/**
+ * Create or compute the URL of a file.
+ *
+ * Can be implemented by the download layer (Jersey) or the upload layer (S3)
+ */
 public interface FileUrlService {
     String url(String uniqueName);
 }
