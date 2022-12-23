024f8686668ba43925ab978254ff9b60992d7aeb
Aurélien Manteaux <amanteaux@coreoz.com>
2022-12-23 20:02:14 +0100

    Review cache + TODO

--- plume-file-web-download-jersey/src/main/java/com/coreoz/plume/file/cache/FileCacheServiceGuava.java
+++ plume-file-web-download-jersey/src/main/java/com/coreoz/plume/file/cache/FileCacheServiceGuava.java
@@ -3,85 +3,63 @@ package com.coreoz.plume.file.cache;
 import com.coreoz.plume.file.service.configuration.FileDownloadConfigurationService;
 import com.coreoz.plume.file.services.FileService;
 import com.coreoz.plume.file.services.metadata.FileMetadata;
-import com.google.common.base.Preconditions;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.cache.Weigher;
 import com.google.common.io.ByteStreams;
-import com.google.common.util.concurrent.UncheckedExecutionException;
 import lombok.SneakyThrows;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.time.Duration;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 @Singleton
 public class FileCacheServiceGuava implements FileCacheService {
 
-    private final LoadingCache<String, byte[]> fileDataCache;
-    private final LoadingCache<String, FileMetadata> fileMetadataCache;
+    private final LoadingCache<String, Optional<byte[]>> fileDataCache;
+    private final LoadingCache<String, Optional<FileMetadata>> fileMetadataCache;
 
     @Inject
     public FileCacheServiceGuava(FileService fileService, FileDownloadConfigurationService configurationService) {
         this.fileDataCache = CacheBuilder.newBuilder()
-            .expireAfterAccess(1, TimeUnit.DAYS)
-            .maximumWeight(configurationService.fileCacheMaxSizeInMb() * 1000000)
-            .weigher((Weigher<String, byte[]>) (k, g) -> g.length)
-            .build(CacheLoader.from(optionalLoaderToExceptionLoader(
-                uid -> fileService.fetchData(uid)
-                    .flatMap(data -> {
-                        try {
-                            return Optional.of(ByteStreams.toByteArray(data));
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    })
-            )));
+            // TODO rendre la durée configurable
+            .expireAfterAccess(Duration.ofDays(1))
+            .maximumWeight(configurationService.fileCacheMaxSize())
+            .weigher((String key, Optional<byte[]> fileData) -> fileData
+                .map(data -> data.length)
+                // Orphan keys handling
+                .orElse(Integer.MAX_VALUE)
+            )
+            .build(CacheLoader.from(uid -> fileService.fetchData(uid)
+                .map(data -> {
+                    try {
+                        return ByteStreams.toByteArray(data);
+                    } catch (IOException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
+            ));
 
         this.fileMetadataCache = CacheBuilder.newBuilder()
-            .expireAfterAccess(1, TimeUnit.DAYS)
+            // TODO rendre la durée configurable
+            .expireAfterAccess(Duration.ofDays(1))
+            // TODO rendre la taille configurable
             .maximumSize(1000)
-            .build(CacheLoader.from(optionalLoaderToExceptionLoader(fileService::fetchMetadata)));
+            .build(CacheLoader.from(fileService::fetchMetadata));
     }
 
+    @SneakyThrows
 	@Override
 	public Optional<byte[]> fetchFileData(String fileUniqueName) {
-		return fetch(fileDataCache, fileUniqueName);
+		return fileDataCache.get(fileUniqueName);
 	}
 
+    @SneakyThrows
 	@Override
 	public Optional<FileMetadata> fetchFileMetadata(String fileUniqueName) {
-		return fetch(fileMetadataCache, fileUniqueName);
+		return fileMetadataCache.get(fileUniqueName);
 	}
-
-	private <K, V> com.google.common.base.Function<K, V> optionalLoaderToExceptionLoader(
-			Function<K, Optional<V>> fileUrlLoader) {
-		return fileUrlLoader.andThen(result -> result.orElseThrow(NotFoundException::new))::apply;
-	}
-
-	@SneakyThrows
-    private <K, V> Optional<V> fetch(LoadingCache<K, V> cache, K cacheKey) {
-		Preconditions.checkNotNull(cache, "Cache has not been initialized");
-        if (cacheKey == null) {
-            return Optional.empty();
-        }
-
-        try {
-            return Optional.of(cache.get(cacheKey));
-        } catch (ExecutionException | UncheckedExecutionException e) {
-            if (e instanceof UncheckedExecutionException && e.getCause() instanceof NotFoundException) {
-                return Optional.empty();
-            }
-            throw e;
-        }
-    }
-
-    private static class NotFoundException extends RuntimeException {
-        private static final long serialVersionUID = 2621559513884831969L;
-    }
 }
