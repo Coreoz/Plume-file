37811449f1ba707b06ff7f17f0724734f15c5dbf
Aurélien Manteaux <amanteaux@coreoz.com>
2023-12-14 11:10:59 +0100

    Add JaxRsFileItemIterator

---
+++ plume-file-web-upload-jersey/src/main/java/com/coreoz/plume/file/streaming/JaxRsFileItemIterator.java
@@ -0,0 +1,38 @@
+package com.coreoz.plume.file.streaming;
+
+import org.apache.commons.fileupload.RequestContext;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Implement RequestContext for JAX-RS {@link ContainerRequestContext}
+ */
+public class JaxRsFileItemIterator implements RequestContext {
+    private final ContainerRequestContext request;
+
+    public JaxRsFileItemIterator(ContainerRequestContext request) {
+        this.request = request;
+    }
+
+    @Override
+    public String getCharacterEncoding() {
+        return StandardCharsets.UTF_8.displayName();
+    }
+
+    @Override
+    public String getContentType() {
+        return request.getMediaType().toString();
+    }
+
+    @Override
+    public int getContentLength() {
+        return request.getLength();
+    }
+
+    @Override
+    public InputStream getInputStream() {
+        return request.getEntityStream();
+    }
+}
